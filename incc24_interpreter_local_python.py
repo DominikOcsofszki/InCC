af36408bcfb0aeb3777975f970335fd74f0bfe94
Dominik Ocsofszki <dominikocsofszki@gmail.com>
2025-01-06 09:54:26 +0100

    setup in local environment, add install script

---
+++ incc24_interpreter_local_python.py
@@ -0,0 +1,21 @@
+#! .venv/bin/python3
+
+import argparse
+
+from compiler import compiler
+from interpreter import interpreter
+
+
+def interpreter_file():
+    argparser = argparse.ArgumentParser(
+        prog="InCC24", description="CLI tool for the InCC24 language"
+    )
+    argparser.add_argument("file", type=str, nargs="?", help="The file to interpret")
+    args = argparser.parse_args()
+    args.repl = False
+
+    interpreter.main(args)
+
+
+if __name__ == "__main__":
+    interpreter_file()
